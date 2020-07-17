ab04a0a891049c7668f94472b6f7e3b5c4fbfa9c
max <karamax@yandex.ru>
2020-07-17 16:03:45 +0300

    final page flight-scoreboard

--- src/js/flight-scoreboard.js
+++ src/js/flight-scoreboard.js
@@ -2,15 +2,37 @@ function flightBoard (){
     if(document.querySelector('.flight-board')){
         const filtersList = document.querySelector('.flight-board-filters-list');
 
-        const arrFilters = filtersList.querySelectorAll()
+        const arrFilters = filtersList.querySelectorAll('.filter-item')
 
-        filtersList.addEventListener('click', (e)=>{
-            console.log(e.target.parentElement)
-            if(e.target.classList.contains('filter-item')){
-            }
-            
-            // if(e.target.classList.contains('filter-item'))
-            // console.log(e.target.parentNode.className=='filter-item today')
+        arrFilters[1].classList.add('active'); // по умолчанию - активный "Сегодня"
+
+        arrFilters.forEach(item =>{
+            item.addEventListener('click', ()=>{
+                arrFilters.forEach(elem =>{
+                    elem.classList.remove('active');
+                })
+                item.classList.add('active');
+            })
+        })
+
+
+        // ------------------ табы
+
+        const formItem_tab = document.querySelectorAll('.form-item_tab');
+
+        formItem_tab[0].classList.add('active');
+        const input = document.querySelectorAll('.input-big');
+        input[0].classList.add('active');
+
+        formItem_tab.forEach((item, i) =>{
+            item.addEventListener('click', ()=>{
+                formItem_tab.forEach((elem, j) =>{
+                    elem.classList.remove('active');
+                    input[j].classList.remove('active');
+                })
+                item.classList.add('active');
+                input[i].classList.add('active');
+            })
         })
 
     }
